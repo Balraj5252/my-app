2d466dee4f9e499084f2f2f1035138be142d0a47
Balraj Hanmanthugari <60377153+Balraj5252@users.noreply.github.com>
2025-07-01 16:18:04 -0700

    xlsx pie View added

---
+++ src/components/Data.js
@@ -0,0 +1,64 @@
+// import SettingsIcon from '@material-ui/icons/Settings';
+// import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+// import BlockIcon from '@material-ui/icons/Block';
+import { BsFiletypeXlsx } from "react-icons/bs";
+import Xlsx from "../images/background/xlsx.jpg";
+export const dataProd =[
+    {
+        id: 1,
+        name: "XLSX",
+        icon: <BsFiletypeXlsx />,
+        bgImage: Xlsx,
+        link: "/xlsx"
+    },
+
+    {
+        id: 2,
+        name: "Base Apps",
+        // icon: <ThumbUpIcon></ThumbUpIcon>,
+        link: "/home"
+    },
+
+    {
+        id: 3,
+        name: "Product Purchase",
+        // icon: <BlockIcon></BlockIcon>,
+        link: ""
+    },
+
+
+];
+
+export const dataOperators = [
+    {
+        id: Math.random(),
+        name: "operator1",
+        objective: "50 parts",
+        inprogress: "15 parts"
+
+    },
+    {
+        id: Math.random(),
+        name: "operator2",
+        objective: "50 parts",
+        inprogress: "12 parts"
+    },
+    {
+        id: Math.random(),
+        name: "operator3",
+        objective: "50 parts",
+        inprogress: "20 parts"
+    }
+];
+
+export const notifications = [
+    {
+        notif1 : "This is a first notification with high priority",
+    },
+    {
+        notif2 : "This is a second notification with medium priority",
+    },
+    {
+        notif3 : "This is a thirs notification with low priority",
+    }
+];
